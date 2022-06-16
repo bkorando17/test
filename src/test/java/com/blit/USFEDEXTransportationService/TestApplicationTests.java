8e23e76a0060929fb0d932071f9ba3920971ec85
brend <brend@DESKTOP-6MB2TIJ.hsd1.va.comcast.net>
2022-06-16 12:33:16 -0400

    First commit from developers

---
+++ src/test/java/com/blit/USFEDEXTransportationService/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blit.USFEDEXTransportationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
